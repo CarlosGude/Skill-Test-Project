6b9b361e596ffc13c45934560552f875ffb7e645
Carlos Gude Sánchez <carlos.sgude@gmail.com>
2023-04-06 14:14:45 +0200

    feat: Add JWT token login with test

--- tests/Api/Security/UserSecurityTest.php
+++ tests/Api/Security/UserSecurityTest.php
@@ -6,9 +6,12 @@ namespace App\Tests\Api\Security;
 
 use App\Tests\Api\AbstractTest;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class UserSecurityTest extends KernelTestCase
 {
+    public const API_LOGIN = 'api/login_check';
+
     protected function setUp(): void
     {
         AbstractTest::setUp();
@@ -21,11 +24,37 @@ class UserSecurityTest extends KernelTestCase
 
     public function testSuccessLogin():void
     {
-        $this->assertTrue(false);
+        self::bootKernel();
+        $container = static::getContainer();
+
+        /** @var HttpClientInterface $httpClient */
+        $httpClient = $container->get(HttpClientInterface::class);
+
+        $response = $httpClient->request('POST',AbstractTest::getBaseUrl().self::API_LOGIN,[
+            'json' => [
+                'email' => 'carlos@gmail.com',
+                'password' => 'carlos@gmail.com'
+            ]
+        ]);
+        $body = $response->toArray();
+        $this->assertEquals(200,$response->getStatusCode());
+        $token = json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $body['token'])[1]))));
+        $this->assertEquals('carlos@gmail.com',$token->username);
     }
 
     public function testFailureLogin():void
     {
-        $this->assertTrue(false);
+        self::bootKernel();
+        $container = static::getContainer();
+
+        /** @var HttpClientInterface $httpClient */
+        $httpClient = $container->get(HttpClientInterface::class);
+        $response = $httpClient->request('POST',AbstractTest::getBaseUrl().self::API_LOGIN,[
+            'json' => [
+                'email' => 'carlos@gmail.com',
+                'password' => 'fail'
+            ]
+        ]);
+        $this->assertEquals(401,$response->getStatusCode());
     }
 }
